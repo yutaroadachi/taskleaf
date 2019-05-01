643c6b83c37a77f556da602ba13c3e02ea6619df
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-05-01 19:05:42 +0900

    ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
 
   resources :tasks
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   namespace :admin do
     resources :users
