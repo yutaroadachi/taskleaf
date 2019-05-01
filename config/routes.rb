87e30fabc1e9ff3f340ec26b8886f0ea744d457c
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-05-01 19:17:48 +0900

    ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
 
   resources :tasks
-  get  '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   namespace :admin do
     resources :users
