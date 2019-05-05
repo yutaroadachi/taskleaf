468657db9da4db7d1d0a1aca43e4003e50785da1
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-05-05 12:07:47 +0900

    タスクの新規作成時に確認画面を追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -50,13 +50,14 @@ describe 'タスク管理機能', type: :system do
     before do
       visit new_task_path
       fill_in '名称', with: task_name
-      click_button '登録する'
+      click_button '確認する'
     end
 
     context '新規作成画面で名称を入力した時' do
       let(:task_name) { '新規作成のテストを書く' }
 
       it '正常に登録される' do
+        click_button '登録'
         expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
       end
     end
