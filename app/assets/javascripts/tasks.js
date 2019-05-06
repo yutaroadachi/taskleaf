7d0f3f47f13c73ff26fd9de3f523d3a839df2ab2
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-05-06 18:49:01 +0900

    タスクの削除のAjax化

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
-  document.querySelectorAll('td').forEach(functuon(td) {
+  document.querySelectorAll('td').forEach(function(td) {
     td.addEventListener('mouseover', function(e) {
       e.currentTarget.style.backgroundColor = '#eff';
     });
@@ -9,3 +9,13 @@ document.addEventListener('turbolinks:load', function() {
     });
   });
 });
+
+// document.addEventListener('turbolinks:load', function() {
+//   document.querySelectorAll('.delete').forEach(function(a) {
+//     a.addEventListener('ajax:success', function() {
+//       var td = a.parentNode;
+//       var tr = td.parentNode;
+//       tr.style.display = 'none';
+//     });
+//   });
+// });
