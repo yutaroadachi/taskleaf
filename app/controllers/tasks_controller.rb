bd4698c6ecbd6ff31888cd3df6c6fe706c8a2c53
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-05-06 11:32:31 +0900

    サンプルジョブを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,8 @@ class TasksController < ApplicationController
     end
 
     if @task.save
-      TaskMailer.creation_email(@task).deliver_now
+      # TaskMailer.creation_email(@task).deliver_now
+      # SampleJob.perform_later
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
